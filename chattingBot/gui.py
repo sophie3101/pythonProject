e4a6b48860a044bfbeebefb979a21a34de5aff7c
huyennguyen <huyen.nguyen.0194@gmail.com>
2023-02-04 17:11:08 -0600

    add scrollbar for output

--- chattingBot/gui.py
+++ chattingBot/gui.py
@@ -1,44 +1,74 @@
 import tkinter as tk
+from tkinter import ttk
 from openAI_davinci_model import OpenAI
 
 class GUI:
-  def __init__(self):
-   
-    self.root = tk.Tk()
+  def __init__(self, root):
+  
+    self.root = root
+
     # resize window frame
     self.root.geometry("750x250")
+    tk.Frame(self.root)
 
     # title of window frame
     self.root.title('OpenAI ChatBot StudyGuide')
 
-    label = tk.Label(self.root, text="What do you want to learn?", font=('Arial', 12))
-    label.pack(pady=10)
+    # input frame
+    self.inputFrame = ttk.Frame(self.root)
+    self.inputFrame.pack(fill="x")
+
+    # input title
+    label = ttk.Label(self.inputFrame, text="What do you want to learn?", font=('Arial', 12))
+    label.pack(pady=5)
 
     # entry for user input
-    self.entry = tk.Entry(self.root, width = 45, font = ('Arial', 12))
-    self.entry.pack(padx=10, pady=10)
+    self.userInput = ttk.Entry(self.inputFrame, font = ('Arial', 12))
+    self.userInput.pack(fill="x")
+
+    # button
+    buttonsFrame = tk.Frame(self.root)
+    buttonsFrame.pack(pady=10)
+
+    button = ttk.Button(buttonsFrame, text = "submit", command=self.clickSubmit)
+    button.pack(side="left")
+
+    button = ttk.Button(buttonsFrame, text = "clear", command=self.reset)
+    button.pack(side="left")
 
     # output
-    self.output_label = tk.Label(self.root, text="",font = ('Arial', 12), wraplength=600, justify=tk.LEFT)
-    self.output_label.pack(padx=10)
+    # scrollbar
+    resultFrame = tk.Frame(self.root)
+    resultFrame.pack(fill="both", expand=True)
 
-    button = tk.Button(self.root, text = "submit", command=self.clickSubmit)
-    button.pack(pady=20)
+    self.scrollbar = ttk.Scrollbar(resultFrame)
+    self.scrollbar.pack(side="right", fill="y")
 
-    self.root.mainloop()
+    self.output = tk.Text(resultFrame, wrap="word")
+    self.output.pack(side="left", fill="both", expand=True)
+
+    self.scrollbar.config(command=self.output.yview)
+    self.output.config(yscrollcommand=self.scrollbar.set)
+    
+  
+  def reset(self):
+    self.userInput.delete(0, 'end')
+    self.output.delete("1.0", tk.END)
     
   def clickSubmit(self):
     # set output as loading... while waiting to get answer from OpenAI
     self.setOutput("loading...")
     self.root.update_idletasks()
 
-    input = self.entry.get()
+    input = self.userInput.get()
     openAI = OpenAI()
     response = openAI.getResponse(input)
     self.setOutput(response)
 
   def setOutput(self, response):
-    self.output_label.config(text=response)
+    self.output.delete("1.0", tk.END)
+    self.output.insert(tk.END, response)
 
-app = GUI()
-
+root = tk.Tk()
+app = GUI(root)
+root.mainloop()
